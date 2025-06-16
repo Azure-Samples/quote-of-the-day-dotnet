57a4d4c2a61892cbf95d3abb1255d5cf7274be3e
April Kwong <aprilk@microsoft.com>
2025-06-15 18:58:56 -0700

    fix pre-down

--- infra/pre-down.ps1
+++ infra/pre-down.ps1
@@ -1,17 +1,22 @@
 # Get the directory of the current script
 $scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
+# Check if user is logged in to Azure CLI
+az account show --output none 2>$null
+if ($LASTEXITCODE -ne 0) {
+    Write-Host "Not logged in to Azure CLI. Please run 'az login' first."
+    exit 1
+}
+
 # Load environment variables from azd env
 $subscriptionId = azd env get-value AZURE_SUBSCRIPTION_ID
 $resourceName = azd env get-value APPCONFIG_RESOURCE_NAME
 $resourceGroup = azd env get-value AZURE_RESOURCE_GROUP
 $experimentationResourceId = "/subscriptions/$subscriptionId/resourceGroups/$resourceGroup/providers/Microsoft.AppConfiguration/configurationStores/$resourceName/experimentation/default"
 
-# Check resource existence by examining the exit code
+# Check experimentation resource existence
 az resource show --ids $experimentationResourceId --api-version 2025-02-01-preview --output none 2>$null
-$resourceExists = $?
-
-if ($resourceExists) {
+if ($LASTEXITCODE -eq 0) {
     Write-Host "Disabling online experimentation for App Configuration resource: $experimentationResourceId"
     az resource delete --ids $experimentationResourceId --api-version 2025-02-01-preview
 } else {
